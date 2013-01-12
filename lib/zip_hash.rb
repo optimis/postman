1b1e277c46be089d941b747685e2a4c227ce501a
Atsuya Takagi <asoftonight@gmail.com>
2013-01-11 17:14:20 -0800

    add ziphash

---
+++ lib/zip_hash.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'fastercsv'
+
+class ZipHash
+  def initialize(path)
+    @data = {}
+
+    loadData(path)
+  end
+
+  def loadData(path)
+    FasterCSV.foreach(path, :col_sep => "\t") do |row|
+      @data[row[1]] = {
+        :city => row[2],
+        :state => row[3],
+        :'state-abbreviation' => row[4]
+      }
+    end
+  end
+
+  def get(zip)
+    @data[zip.to_s]
+  end
+end
