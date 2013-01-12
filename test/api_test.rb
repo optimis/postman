d1d1a4febe4d34fde2e591a60dbff20774c51cfa
Atsuya Takagi <asoftonight@gmail.com>
2013-01-11 17:13:37 -0800

    add test

---
+++ test/api_test.rb
@@ -0,0 +1,32 @@
+require 'api'
+
+require 'test/unit'
+require 'rack/test'
+
+require 'json'
+
+ENV['RACK_ENV'] = 'test'
+
+class ApiTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_returns_success_response
+    get '/', :zip => '90293'
+
+    response = JSON.parse(last_response.body)
+    assert_equal 200, last_response.status
+    assert_equal 'Playa del Rey', response['city']
+    assert_equal 'California', response['state']
+    assert_equal 'CA', response['state-abbreviation']
+  end
+
+  def test_it_returns_error_response
+    get '/', :zip => 'notvalid'
+
+    assert_not_equal 200, last_response.status
+  end
+end
