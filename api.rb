dd607436fff7cc380a468e72af416c250bf88ef3
Atsuya Takagi <asoftonight@gmail.com>
2013-01-12 11:38:07 -0800

    add some error handlings

--- api.rb
+++ api.rb
@@ -6,13 +6,30 @@ require './lib/zip_hash'
 
 module Postman
   class API < Sinatra::Base
+    # load data
     p 'Loading zip data...'
     zip_hash = ZipHash.new('./data/US.txt')
     p 'Done!'
 
+    before do
+      content_type 'application/json', :charset => 'utf-8'
+    end
+
+    error 403 do
+      MultiJson.dump({ :error => 'Forbidden' })
+    end
+
+    error 404 do
+      MultiJson.dump({ :error => 'Not Found' })
+    end
+
     get '/:zip' do
       data = zip_hash.get(params[:zip])
-      MultiJson.dump(data)
+      if data
+        MultiJson.dump(data)
+      else
+        403
+      end
     end
   end
 end
